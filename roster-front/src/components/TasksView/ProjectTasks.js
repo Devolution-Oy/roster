37476f034824e1384d99a0a0fd5da1a6c3966d4b
Mikko Kurkela <mikko.kurkela@devolution.fi>
2020-07-10 18:07:49 +0300

    Initial work to fetch tasks from github

--- roster-front/src/components/TasksView/ProjectTasks.js
+++ roster-front/src/components/TasksView/ProjectTasks.js
@@ -1,6 +1,6 @@
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getIssues } from '../GithubRequests';
 
 class ProjectTasks extends Component {
   constructor(props) {
@@ -9,27 +9,54 @@ class ProjectTasks extends Component {
       tasks: null,
       loading: null,
       error: null,
-      name: props.name
+      name: props.name,
+      user: props.user
     };
+  }
+
+  componentDidMount() {
+    this.setState({loading: 'Fetching tasks...'});
+    getIssues(this.state.name, this.state.user).then(res => {
+      this.setState({tasks: res.data});
+      this.setState({loading: null});
+    }).catch(err => {
+      console.log(err.message);
+      this.setState({loading: null});
+    });
   }
 
   render() {
     const name = this.state.name;
     const id = this.state.name + '_tasks';
+    const loading = this.state.loading;
+    const tasks = this.state.tasks;
+
+    if (loading) {
+      return (
+        <div id={id}>
+          <h3>{name}</h3>
+          <p>{loading}</p>
+        </div>
+      );
+    }
     // TODO: Get project's tasks from github
     return (
       <div id={id}>
         <h3>{name}</h3>
-        <label>Task1</label>
-        <label>Task2</label>
-        <label>Task3</label>
+        {tasks ? tasks.map((task, i) => {
+          return (
+            <label key={i}>{task.title}</label>
+          );
+        }) : <p>No open tasks</p>
+        }
       </div>
     );
   }
 }
 
 ProjectTasks.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
 };
 
 export default ProjectTasks;
